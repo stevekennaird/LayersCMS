84141781cd3cda07fcf3564babe5cfccc01110d2
stevekennaird <steve@steveonthefly.co.uk>
2012-10-27 18:20:52 +0100

    More work on page management and the admin area

--- Website/LayersCMS.MvcApp/Content/admin/scripts/layers-cms-scripts.js
+++ Website/LayersCMS.MvcApp/Content/admin/scripts/layers-cms-scripts.js
@@ -5,7 +5,17 @@ layersCms.initTooltips = function () {
     $('a.tip, .tips a').tipsy({ fade: true, gravity: 'n' });
 };
 
+layersCms.initPagePreviews = function() {
+    $('a.page-preview').fancybox({
+        width: 1100,
+        minHeight: 500,
+        title: function () {
+            return '<em>Page Preview:</em> ' + $(this).attr('data-page-title');
+        }
+    });
 
+    $('a.fancybox').fancybox();
+};
 
 
 
@@ -23,6 +33,7 @@ layersCms.initTooltips = function () {
 
 layersCms.init = function() {
     layersCms.initTooltips();
+    layersCms.initPagePreviews();
 };
 
 
